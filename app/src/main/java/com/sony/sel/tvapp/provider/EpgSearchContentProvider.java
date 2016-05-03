2f979f5bcc6b15aea91cf72f8a7da0760d3594c6
Peter Vanags <peterv@Peters-MBP.attlocal.net>
2016-05-03 11:17:10 -0700

    Implement EPG search ContentProvider

---
+++ app/src/main/java/com/sony/sel/tvapp/provider/EpgSearchContentProvider.java
@@ -0,0 +1,45 @@
+package com.sony.sel.tvapp.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+/**
+ * Android content provider for searching EPG data.
+ */
+public class EpgSearchContentProvider extends ContentProvider {
+  @Override
+  public boolean onCreate() {
+    return true;
+  }
+
+  @Nullable
+  @Override
+  public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public String getType(Uri uri) {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public Uri insert(Uri uri, ContentValues values) {
+    return null;
+  }
+
+  @Override
+  public int delete(Uri uri, String selection, String[] selectionArgs) {
+    return 0;
+  }
+
+  @Override
+  public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+    return 0;
+  }
+}
