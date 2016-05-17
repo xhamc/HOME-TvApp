4a75d286f901fc0836a5738cc59dff1bc838f5ea
Peter Vanags <peterv@PeteBook-Air.local>
2016-05-17 10:19:50 -0700

    Update EPG caching to be more efficient determining what’s already cached.

--- app/src/main/java/com/sony/sel/tvapp/util/EpgCachingTask.java
+++ app/src/main/java/com/sony/sel/tvapp/util/EpgCachingTask.java
@@ -61,30 +61,24 @@ public class EpgCachingTask extends AsyncTask<Void, Void, Void> {
     Calendar calendar = Calendar.getInstance();
 
     // cache today's programs
-    int programCount = cachePrograms(calendar.getTime(), channelIds);
-    if (programCount == 0) {
-      Log.e(TAG, "No EPG data found.");
-      return null;
-    } else if (isCancelled()) {
+    boolean cached = cachePrograms(calendar.getTime(), channelIds);
+    if (!cached || isCancelled()) {
       return null;
     }
 
     // cache yesterday's programs
     calendar.add(Calendar.DAY_OF_MONTH, -1);
-    programCount = cachePrograms(calendar.getTime(), channelIds);
-    if (programCount == 0) {
-      Log.e(TAG, "No EPG data found.");
-      return null;
-    } else if (isCancelled()) {
+    cached = cachePrograms(calendar.getTime(), channelIds);
+    if (!cached || isCancelled()) {
       return null;
     }
 
     // cache ahead
     calendar.add(Calendar.DAY_OF_MONTH, 2);
     do {
-      programCount = cachePrograms(calendar.getTime(), channelIds);
+      cached = cachePrograms(calendar.getTime(), channelIds);
       calendar.add(Calendar.DAY_OF_MONTH, 1);
-    } while (programCount > 0 && !isCancelled());
+    } while (cached && !isCancelled());
 
     return null;
   }
@@ -94,14 +88,15 @@ public class EpgCachingTask extends AsyncTask<Void, Void, Void> {
    *
    * @param date       Date to cache.
    * @param channelIds Channel IDs to cache.
-   * @return Total number of items cached (or found already cached).
+   * @return true if items were cached, false if no EPG items are available for the channels/date requested.
    */
-  private int cachePrograms(Date date, List<String> channelIds) {
+  private boolean cachePrograms(Date date, List<String> channelIds) {
     final DateFormat format = new SimpleDateFormat("M-d");
 
     DateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-    Log.d(TAG, "Cache programs for " + isoFormat.format(date) + ".");
+    Log.d(TAG, "Cache programs for local date " + isoFormat.format(date) + ".");
 
+    // create UTC dates for start & end of day
     Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
     calendar.setTime(date);
     calendar.set(Calendar.HOUR_OF_DAY, 0);
@@ -113,23 +108,30 @@ public class EpgCachingTask extends AsyncTask<Void, Void, Void> {
     calendar.add(Calendar.MILLISECOND, -1);
     Date end = calendar.getTime();
 
-    isoFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-    Log.d(TAG, "Searching cache from " + isoFormat.format(start) + " to " + isoFormat.format(end) + ".");
+    // convert format to UTC before parsing the day string
+    format.setTimeZone(TimeZone.getTimeZone("UTC"));
+    final String day = format.format(start);
+    Log.d(TAG, "UTC date is " + day + ".");
 
+    // search the cache for programs in the EPG right before midnight UTC
+    // if they are found for all channels we assume that the entire day has been cached
+    int cacheCount = dlnaCache.countEpgItems(
+        udn,
+        channelIds,
+        end,
+        end
+    );
+    if (cacheCount == channelIds.size()) {
+      Log.d(TAG, "EPG data already cached for " + day + ".");
+      return true;
+    }
+
+    // need to cache EPG for this day
     int programCount = 0;
-
     for (String channelId : channelIds) {
-
-      // convert to UTC before parsing out date
-      format.setTimeZone(TimeZone.getTimeZone("UTC"));
-      final String day = format.format(start);
-
-      List<String> idList = new ArrayList<>();
-      idList.add(channelId);
-
       Log.d(TAG, "Caching EPG data for channel " + channelId + " " + day + ".");
 
-      // get container
+      // get container ID for channel & day
       String parentId = "0/EPG/" + channelId + "/" + day;
 
       // get EPG data from server
@@ -137,11 +139,10 @@ public class EpgCachingTask extends AsyncTask<Void, Void, Void> {
       programCount += programs.size();
 
       if (isCancelled()) {
-        return programCount;
+        return programCount > 0;
       }
-
     }
-    return programCount;
+    return programCount > 0;
   }
 
   @Override
